888fe5f510642eeff399fd4bb5e42f8af13e8702
Josman Pérez Expósito <josman.perez@udc.es>
2019-07-13 16:32:00 +0200

    First commit of Github Noops Challenge for Hexbot

---
+++ hexbotios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hexbotios
+//
+//  Created by Josman Pérez Expósito on 13/07/2019.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
